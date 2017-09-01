81fab15236469f5f73ddb56e4fe9cd8a18067721
Wouter Steenkist <wouter@mailcampaigns.nl>
2017-09-01 13:41:59 +0200

    no message

--- Observer/MailCampaignsAPIConfig.php
+++ Observer/MailCampaignsAPIConfig.php
@@ -29,7 +29,7 @@ class MailCampaignsAPIConfig implements ObserverInterface
 		\Magento\Framework\App\ResourceConnection $Resource,
         Logger $logger
     ) {
-		$this->version 		= '2.0.12';
+		$this->version 		= '2.0.13';
 		$this->logger 		= $logger;
 		$this->helper 		= $dataHelper;
 		$this->mcapi 		= $mcapi;
